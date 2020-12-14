a8b2323195944828a46e158dd0a570ec2e879fa4
Kushalkhemka <Kushalkhemka559@gmail.com>
2020-12-14 22:03:56 +0530

    modified

--- sketch.js
+++ sketch.js
@@ -5,10 +5,14 @@ const Body=Matter.Body;
 const Constraint=Matter.Constraint;
 const MouseConstraint=Matter.MouseConstraint;
 
+var engine,world;
 var drops,umbrella;
 let umbrella_anim;
 let maxDrops=100;
 var thunderbolt;
+var drops=[];
+var num;
+var thunderframe;
 
 var thunderbolt_img1,thunderbolt_img2,thunderbolt_img3,thunderbolt_img4;
 
@@ -33,28 +37,36 @@ function setup(){
    world=engine.world;
 
   
-   thunderbolt=createSprite(200,300,100,100);
-   thunderbolt.addImage(thunderbolt_img1);
+  
+   //thunderbolt.addImage(thunderbolt_img1);
     
-   umbrella=new Umbrella(200,530,400,400);
+   umbrella=new Umbrella(200,500);
 
+   
+   if(frameCount%150===0)
+   {
+    for(i=0;i<maxDrops;i++)
+   {
+       drops.push(new Drop(random(0,400),random(0,400)));
+   }
+ }
    
 }
 
 function draw(){
-    
+    //console.log(num);
+    Engine.update(engine);
+
     //To assign the background
     background("black");
 
-    console.log(num);
-    Engine.update(engine);
-
-     for(i=0;i<maxDrops;i++)
+    console.log(thunderbolt);
+    console.log(frameCount);
+    if(frameCount%80===0)
     {
-        drops.push(new Drop(random(0,400),random(0,400)));
-    }
-
-    var num=Math.round(random(1,4)); 
+    thunderframe=frameCount;  
+    thunderbolt=createSprite(random(10,370),random(10,30),10,10);
+    num=Math.round(random(1,4)); 
     switch(num)
     {
         case 1: thunderbolt.addImage(thunderbolt_img1);
@@ -65,14 +77,29 @@ function draw(){
             break;
         case 4: thunderbolt.addImage(thunderbolt_img4);
             break;
+        default: break;
     }
+    //randomizing thunderbolt
+    thunderbolt.scale=random(0.3,0.6);
+}
 
-    
+//destorying thunderbolt after 10 frames from its creation frame count
+if(thunderframe+10===frameCount && thunderbolt)
+{
+    thunderbolt.destroy();
+}
+
+
+ for(var i=0;i<maxDrops;i++)
+ {
+     drops[i].showDrops();
+     drops[i].update();
+ }   
   
  //  drops.update();
 
     umbrella.display();
     
-   
+ drawSprites();   
 }   
 
